ce133abd4f30977d610f0664ce76e0b4e1620a15
Frederic Forster <frederic@appdafuer.com>
2017-04-04 15:54:30 +0200

    Code Refactored

--- TimeTracker/AlertGenerator.swift
+++ TimeTracker/AlertGenerator.swift
@@ -1,5 +1,5 @@
 //
-//  NewDescription.swift
+//  AlertGenerator.swift
 //  TimeTracker
 //
 //  Created by Frederic Forster on 13/03/2017.
@@ -9,19 +9,9 @@
 import Foundation
 import UIKit
 
-class NewDescription {
+class AlertGenerator {
     
-    private var beschreibung: String = ""
-    let viewController:UIViewController
-    let completion:(String) -> ()
-    
-    init(viewController:UIViewController, completion:@escaping (String) -> ()){
-        self.viewController = viewController
-        self.completion = completion
-        fireAlert()
-    }
-    
-    func fireAlert() {
+    static func showTextInputAlert(onViewController viewController:UIViewController, completion:@escaping (String) -> ()) {
         let alert = UIAlertController(title: "Neue Beschreibung", message: "Geben sie eine neue Beschreibung ein:", preferredStyle: .alert)
         
         alert.addTextField{ (textField) in
@@ -30,17 +20,11 @@ class NewDescription {
         
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in
             let textField = alert.textFields![0]
-            self.beschreibung = textField.text!
-            self.completion(textField.text!)
+                completion(textField.text!)
         }))
         
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {action in}))
         
         viewController.present(alert, animated: true, completion: nil)
     }
-    
-    func getDescription() -> String {
-        return beschreibung
-    }
-    
 }
